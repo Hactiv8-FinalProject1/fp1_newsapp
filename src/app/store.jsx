446ca226e9415e80e89efe768795c53f1038205a
Dika Rifai <twdrr02@gmail.com>
2023-05-09 16:43:26 +0800

    Fitur Search

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import covid19Reducer from "../features/reduser/covid19";
 import indonesiaReducer from "../features/reduser/indonesia";
 import programmingReducer from "../features/reduser/programming";
+import searchSlice from "../features/reduser/searchSlice";
 
 export const store = configureStore({
   reducer: {
-    covid19 : covid19Reducer,
-    indonesia : indonesiaReducer,
-    programming : programmingReducer,
+    covid19: covid19Reducer,
+    indonesia: indonesiaReducer,
+    programming: programmingReducer,
+    search: searchSlice,
   },
 });
---
+++ src/features/reduser/searchSlice.jsx
@@ -0,0 +1,50 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  search: "",
+  result: [],
+  isLoading: false,
+};
+
+const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    setSearch: (state, action) => {
+      state.search = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getSeacrh.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(getSeacrh.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.result = action.payload;
+        // console.log(action.payload);
+      })
+      .addCase(getSeacrh.rejected, (state, action) => {
+        state.isLoading = false;
+        console.log("error", action.error.message);
+      });
+  },
+});
+
+export const { setSearch } = searchSlice.actions;
+export default searchSlice.reducer;
+
+export const getSeacrh = createAsyncThunk(
+  "search/getSeacrh",
+  async (search) => {
+    try {
+      const resp = await axios.get(
+        `${process.env.REACT_APP_BASE_URL}everything?q="${search}"&apiKey=${process.env.REACT_APP_API_KEY}`
+      );
+      return resp.data.articles;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
